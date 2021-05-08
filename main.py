b05eca648d9e10f73344b131026614e03481b55c
Michael Mudge <mmudge@sailthru.com>
2021-05-08 17:17:05 +1200

    Look through Load metrics 10 times with a sleep, display image.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #!python3
 import requests
+import subprocess
 import sys
+from time import sleep
 from PIL import Image
 
 
@@ -12,17 +14,52 @@ def main():
 	if len(sys.argv) > 1:
 		ip = sys.argv[1]
 
+	print("Running on", sys.platform)
+	pi = True
+	if sys.platform == 'darwin':
+		pi = False
+
 	print('Looking at host', ip)
 
-	response = requests.get("http://" + ip + "/data.json")
+	cpuHistory = []
+	proc = None
+	for i in range(10):
+		response = requests.get("http://" + ip + "/data.json")
 
-	data = response.json()
+		data = response.json()
 
-	for k in data:
-		print(k)
+		print('Keys', data.keys())
+		pc = data.get('Children')[0]
 
-	image = Image.open('image.png')
-	image.show()
+		print("Showing data for PC named", pc.get('Text'))
+		
+		for component in pc.get('Children'):
+			name = component.get('Text')
+			for section in component.get('Children'):
+				if section.get('Text') == 'Load':
+					for v in section.get("Children"):
+						if v.get('Text') == 'CPU Total':
+							# This is a primary metric
+							print("CPU Load =", v.get('Value'))
+							cpuPercent = float(v.get('Value')[:-1].strip())
+							print("float()", cpuPercent)
+							cpuHistory.append(cpuPercent)
+
+						print(component.get('Text'), "> Load >", v.get('Text'), v.get('Value'))
+
+		# Pick a face and display it.
+		if pi:
+			if proc:
+				proc.terminate()
+			proc = subprocess.Popen('feh --hide-pointer -Z -F -x -q -B white ~/projects/piface/image.png'.split(' '))
+		else:
+			# Laptop development
+			print("Displaying image.png")
+
+		sleep(1)
+
+
+	print("CPU usage over time", cpuHistory)
 
 	print("Finished piface")
 
